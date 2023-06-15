8b333e82297d388f4af6343fa61724ea9e882dbe
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2023-06-15 16:56:03 +0000

    Initial Commit

---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.lscore = 0
+        self.rscore = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.lscore, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.rscore, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.lscore += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.rscore += 1
+        self.update_scoreboard()
